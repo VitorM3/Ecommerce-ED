f5e66c35e7570a4271c40852fc8ed2c6e2802f85
Vitor Loch <vitor.rosa@innovaconnect.com.br>
2024-06-26 20:33:43 -0300

    FIX: Sql

--- database_dimensional/database.sql
+++ database_dimensional/database.sql
@@ -2,12 +2,11 @@ CREATE TABLE dim_client(
     client_id INT PRIMARY KEY,
     "name" VARCHAR NOT NULL,
     email VARCHAR NOT NULL,
-    document VARCHAR NOT NULL,
+    document VARCHAR NOT NULL
 );
 
 CREATE TABLE dim_address(
     address_id INT PRIMARY KEY,
-    cep VARCHAR NOT NULL,
     uf VARCHAR NOT NULL,
     city VARCHAR NOT NULL,
     country VARCHAR NOT NULL
@@ -19,14 +18,16 @@ CREATE TABLE dim_product_category(
 );
 
 CREATE TABLE fact_order(
-    dim_client_id INT PRIMARY KEY,
-    dim_address_id INT PRIMARY KEY,
-    dim_category_product_id INT PRIMARY KEY,
+    dim_client_id INT NOT NULL,
+    dim_address_id INT NOT NULL,
+    dim_category_product_id INT NOT NULL,
+    order_id INT NOT NULL,
     price FLOAT NOT NULL,
     payment_method VARCHAR NOT NULL,
     "status" VARCHAR NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    CONSTRAINT fk_dim_client REFERENCES dim_client (client_id),
-    CONSTRAINT fk_dim_address REFERENCES dim_address (address_id),
-    CONSTRAINT fk_dim_category_product REFERENCES dim_product_category (category_id)
+    PRIMARY KEY (dim_client_id,dim_address_id,dim_category_product_id,order_id),
+    CONSTRAINT  fk_dim_client FOREIGN KEY (dim_client_id) REFERENCES dim_client (client_id),
+    CONSTRAINT  fk_dim_address FOREIGN KEY (dim_address_id) REFERENCES dim_address (address_id),
+    CONSTRAINT  fk_dim_category_product FOREIGN KEY (dim_category_product_id) REFERENCES dim_product_category (category_id)
 );
